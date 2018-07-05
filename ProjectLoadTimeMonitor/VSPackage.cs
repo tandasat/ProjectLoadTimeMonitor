c08d8b4a8a8a34c147788e2bfd6ce0b17af7433a
Satoshi Tanda <tanda.sat@gmail.com>
2018-07-04 19:09:04 -0700

    add initial files

---
+++ ProjectLoadTimeMonitor/VSPackage.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Events;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace ProjectLoadTimeMonitor
+{
+    [Guid("d074fa83-c523-495c-bcb3-ca8db1fed88b")]
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+
+    //
+    // The following line will schedule the package to be initialized when a
+    // solution is being opened
+    //
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionOpening_string)]
+
+    //
+    // Not using AsyncPackage as such an extension gets loaded too late,
+    // regardless of what UI context is specified.
+    //
+    public sealed class VSPackage : Package
+    {
+        private const string k_ExtensionName = "Project Load Time Monitor";
+        private readonly Dictionary<Guid, long> m_LoadingTimes = new Dictionary<Guid, long>();
+        private IVsOutputWindowPane m_OutputWindow;
+
+        public VSPackage()
+        {
+            //
+            // Subscribe interesting events.
+            //
+            SolutionEvents.OnBeforeOpenProject += HandleBeforeOpenProjectEvent;
+            SolutionEvents.OnAfterOpenProject += HandleAfterOpenProjectEvent;
+
+            //
+            // FIXME: I was unable to make this pattern work:
+            //  ThreadHelper.JoinableTaskFactory.Run(async delegate {
+            //      await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            //      DoSomething();
+            //  });
+            // The DoSomething part did not get executed for some reasons.
+            //
+            //ThreadHelper.ThrowIfNotOnUIThread(k_ExtensionName);
+
+            //
+            // Get the OutputWindow for logging.
+            //
+            var paneGuid = VSConstants.OutputWindowPaneGuid.GeneralPane_guid;
+            var outputWindow = (IVsOutputWindow)GetGlobalService(typeof(SVsOutputWindow));
+            int result = outputWindow.CreatePane(paneGuid, k_ExtensionName, 1, 0);
+            if (ErrorHandler.Failed(result))
+            {
+                Trace.WriteLine(string.Format("CreatePane failed : {0:X8}", result),
+                                k_ExtensionName);
+                return;
+            }
+
+            result = outputWindow.GetPane(paneGuid, out m_OutputWindow);
+            if (ErrorHandler.Failed(result))
+            {
+                Trace.WriteLine(string.Format("GetPane failed : {0:X8}", result),
+                                k_ExtensionName);
+                return;
+            }
+
+            OutputLine("Initialization completed.");
+            OutputLine("---------------------------------------------");
+            OutputLine(string.Format("{0,-50}, {1,-20},", "Project Name", "Elapsed Time (ms)"));
+        }
+
+        private void OutputLine(string Message)
+        {
+            m_OutputWindow.OutputString(Message + Environment.NewLine);
+            Trace.WriteLine(Message, k_ExtensionName);
+        }
+
+        private void HandleBeforeOpenProjectEvent(object sender, BeforeOpenProjectEventArgs e)
+        {
+            //
+            // A project is being loaded. Store the current time.
+            //
+            m_LoadingTimes.Add(e.Project, DateTimeOffset.Now.ToUnixTimeMilliseconds());
+        }
+
+        private void HandleAfterOpenProjectEvent(object sender, OpenProjectEventArgs e)
+        {
+            //
+            // A project has been loaded. Save the current time.
+            //
+            long finishTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+            //ThreadHelper.ThrowIfNotOnUIThread(k_ExtensionName);
+
+            //
+            // Get the project name.
+            //
+            int result = e.Hierarchy.GetProperty(VSConstants.VSITEMID_ROOT,
+                                                 (int)__VSHPROPID.VSHPROPID_Name,
+                                                 out Object nameAsObject);
+            if (ErrorHandler.Failed(result))
+            {
+                OutputLine(string.Format("GetProperty failed : {0:X8}", result));
+                return;
+            }
+            var projectName = (string)nameAsObject;
+
+            //
+            // Get the project GUID.
+            //
+            result = e.Hierarchy.GetGuidProperty(VSConstants.VSITEMID_ROOT,
+                                                 (int)__VSHPROPID.VSHPROPID_ProjectIDGuid,
+                                                 out Guid projectGuid);
+            if (ErrorHandler.Failed(result))
+            {
+                OutputLine(string.Format("GetGuidProperty failed : {0:X8}", result));
+                return;
+            }
+
+            //
+            // Pop the start time for the project and log it.
+            //
+            if (!m_LoadingTimes.TryGetValue(projectGuid, out long startTime))
+            {
+                //
+                // The project called "Miscellaneous Files" is reported as an
+                // empty GUID during HandleBeforeOpenProjectEvent. Check if this
+                // completion is for the project.
+                //
+                projectGuid = Guid.Empty;
+                if (!m_LoadingTimes.TryGetValue(projectGuid, out startTime))
+                {
+                    OutputLine(string.Format("TryGetValue failed : {0}", projectName));
+                    return;
+                }
+            }
+
+            m_LoadingTimes.Remove(projectGuid);
+
+            long elapsedTime = finishTime - startTime;
+            OutputLine(string.Format("{0,-50}, {1,20},", projectName, elapsedTime));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# ProjectLoadTimeMonitor
+Project Load Time Monitor
+==========================
+
+Introduction
+-------------
+
+Project Load Time Monitor is a Visual Studio extension that measures load time of each project when a solution file is opened. This extension aims to help developers identify projects slowing down solution load.
+
+Developers often experience unacceptable slowness when opening solution files with 100+ projects on Visual Studio. While one of the easiest solutions to the issue is to separate or unload some rarely used projects, this cannot be done efficiently because there is no built-in way to measure which projects are making solution load slow.
+
+This extension measures time needed to complete load of each project, and outputs them in the CSV format to assist further manual analysis.
+
+
+Examples
+---------
+
+The results are displayed on the Output Window in the chronological order once all projects are loaded as show below.
+
+![OutputWindow](/Images/ExampleOutput.png)
+
+Here is another example results taken from a real-world solution which consisted of 400+ projects. The below pie chart was created with Excel with the result of this extension and shows that the only top five projects are responsible for one third of the entire solution time. I was able to save 30% of load time by just unloading four of those projects from the solution.
+
+![SlowProjects](/Images/SlowProjects.png)
+
+
+Supported Platforms
+--------------------
+- Visual Studio 2017
+
+
+Download
+---------
+
+- Visual Studio Marketplace
+  - https://marketplace.visualstudio.com/items?itemName=SatoshiTanda.ProjectLoadTimeMonitor
